e26656dfeef161dd167bbdcb2dd8b51ecd388846
Anton Johansson <97antjoh@gmail.com>
2020-11-16 18:45:46 +0000

    forgot some files

---
+++ projects/bestmf_fig1/solve.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+./bestmf_fig1 -g -4.0 -o out1
+./bestmf_fig1 -g -3.5 -o out2
+./bestmf_fig1 -g -2.5 -o out3
+./bestmf_fig1 -g -1.5 -o out4
+./bestmf_fig1 -g -0.5 -o out5
+./bestmf_fig1 -g  0.5 -o out6

b3b0a9a3777d5b7388e781fc03dafb661d144858
Anton Johansson <97antjoh@gmail.com>
2021-02-17 01:29:42 +0100

    sync

--- src/sbmf/math/hermite_integrals.c
+++ src/sbmf/math/hermite_integrals.c
@@ -1,13 +1,11 @@
 f64 hermite_integral_4(u32 i, u32 j, u32 k, u32 l) {
-	//if ((i+j+k+l) % 2 != 0)
-	//	return 0.0;
+	if ((i+j+k+l) % 2 != 0)
+		return 0.0;
 
 	u32 m_max = (i < j) ? i : j;
 
 	f128 sum = 0.0;
 	for (u32 m = 0; m <= m_max; ++m) {
-		if (((i+j-2*m)+k+l) % 2 != 0)
-			continue;
 
 		f128 df1 = double_factorial_128( (i+j-2*m) + k - l - 1);
 		f128 df2 = double_factorial_128( (i+j-2*m) - k + l - 1);
@@ -17,7 +15,7 @@ f64 hermite_integral_4(u32 i, u32 j, u32 k, u32 l) {
 		f128 f2 = factorial_128(i-m);
 		f128 f3 = factorial_128(j-m);
 
-		sum += (pow(2,m)/f1)*(df1/f2)*(df2/f3)*df3;
+		sum += (pow(2,m)/f1)*(1/f2)*(1/f3)*(df1*df2*df3);
 	}
 
 	const f64 sqrt_omega_over_2pi = sqrt(OMEGA/(2.0*M_PI));

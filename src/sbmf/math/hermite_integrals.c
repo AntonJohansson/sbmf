cfff608d3fe1aba5855cca440f4a3fe9bbacd5e4
Anton Johansson <97antjoh@gmail.com>
2021-02-17 01:22:45 +0100

    sync

--- src/sbmf/math/hermite_integrals.c
+++ src/sbmf/math/hermite_integrals.c
@@ -4,11 +4,6 @@ f64 hermite_integral_4(u32 i, u32 j, u32 k, u32 l) {
 
 	u32 m_max = (i < j) ? i : j;
 
-	f128 fi = factorial_128(i);
-	f128 fj = factorial_128(j);
-	f128 fk = factorial_128(k);
-	f128 fl = factorial_128(l);
-
 	f128 sum = 0.0;
 	for (u32 m = 0; m <= m_max; ++m) {
 
@@ -20,17 +15,14 @@ f64 hermite_integral_4(u32 i, u32 j, u32 k, u32 l) {
 		f128 f2 = factorial_128(i-m);
 		f128 f3 = factorial_128(j-m);
 
-
-		sum +=
-			((pow(2,m)/f1)*sqrt(fi/pow(2,i)))
-			*((df1/f2)*sqrt(fj/pow(2,j)))
-			*((df2/f3)*sqrt(fk/pow(2,k)))
-			*df3*sqrt(fl/pow(2,l));
+		sum += (pow(2,m)/f1)*(df1/f2)*(df2/f3)*df3;
 	}
 
 	const f64 sqrt_omega_over_2pi = sqrt(OMEGA/(2.0*M_PI));
+	f128 f1 = factorial_128(i);
+	f128 f2 = factorial_128(j);
+	f128 f3 = factorial_128(k);
+	f128 f4 = factorial_128(l);
 
-	//return sqrt_omega_over_2pi*sqrt(((f1/f3)*(f2/f4)))/pow(2,(i+j+k+l)/2)*sum;
-	return sqrt_omega_over_2pi
-		*sum;
+	return sqrt_omega_over_2pi*sqrt(((f1/f3)*(f2/f4)))/pow(2,(i+j+k+l)/2)*sum;
 }
